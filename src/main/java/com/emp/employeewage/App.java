2f26d0042e3c141c79cc4322f63ecfe7677dc839
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-09-17 15:39:17 +0530

    Initial Commit

---
+++ src/main/java/com/emp/employeewage/App.java
@@ -0,0 +1,13 @@
+package com.emp.employeewage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation Program" );
+    }
+}
