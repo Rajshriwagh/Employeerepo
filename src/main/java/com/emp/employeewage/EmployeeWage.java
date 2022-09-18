26ccb070396359f33d40ad2fcdf1156a341dcbb4
Rajshriwagh <mahajan.rajshri16@gmail.com>
2022-09-18 19:45:25 +0530

    Daily wage calculation

--- src/main/java/com/emp/employeewage/EmployeeWage.java
+++ src/main/java/com/emp/employeewage/EmployeeWage.java
@@ -3,15 +3,23 @@ package com.emp.employeewage;
 import java.util.Random;
 
 public class EmployeeWage {
+
+	int empRatePerHr = 20;
+	int empHr;
+	int empWage;
+
 	int present = 1;
 	Random random = new Random();
 	int attend = random.nextInt(2);
 
 	public void employeeAttendance() {
 		if (present == attend) {
-			System.out.println("Employee is present");
+			empHr = 8;
+
 		} else {
-			System.out.println("Employee is absent");
+			empHr = 0;
 		}
+		empWage = empHr * empRatePerHr;
+		System.out.println("Employee Daily wage is " + empWage);
 	}
 }
